59a656138fc2e55a90b79f83041ff103589043e2
JoaoSerafim1 <joaolucascademico@gmail.com>
2024-09-07 18:26:33 -0300

    feature: Added matrix union function
    change: Adapted move functions and main program tests for relative positions of the 4x4 tetris piece
    
    feature: Added many colors for the exibition function
    
    test: Main program generates a random color index for the piece and translocates it starting rom the top-center all the way down to the bottom-center, and then trying to go out of bounds (should fail)

--- game.c
+++ game.c
@@ -3,116 +3,207 @@
 # include <unistd.h>
 #include <intelfpgaup/video.h>
 
-int preenche_zero(int t[10][24]){
-    int cont0;
-    int cont1;
-    for(cont0 = 0; cont0 < 10; cont0++)
-  {
-        for(cont1 = 0; cont1 < 24; cont1++){
-            t[cont0][cont1] = 0;
-        }
-  }
-  return 0;
+
+//Funcao que preenche com 0 todas as celulas de uma matriz 10x24
+int preenche_zero_10_x_24(int tela[10][24]) {
+  int cont0;
+  int cont1;
   
+  for(cont0 = 0; cont0 < 10; cont0++) {
+    
+    for(cont1 = 0; cont1 < 24; cont1++) {
+      tela[cont0][cont1] = 0;
+    }
+  }
+
+  return 0;
 }
 
-int imprime_tela(int t[10][20]){
+//Funcao que preenche com 0 todas as celulas de uma matriz 4x4
+int preenche_zero_4_x_4(int tela[4][4]) {
     int cont0;
     int cont1;
-    for(cont0 = 0; cont0 < 10; cont0++)
-  {
-        for(cont1 = 0; cont1 < 20; cont1++){
-            printf("%d", t[cont0][cont1]);
-        }
-        printf("\n");
-  }
-  return 0;
   
+  for(cont0 = 0; cont0 < 4; cont0++) {
+    
+    for(cont1 = 0; cont1 < 4; cont1++){
+      tela[cont0][cont1] = 0;
+    }
+  }
+
+  return 0;
 }
 
+
+//Funcao que une (modifica matriz tela) uma matriz com posicao absoluta de tamanho 10x24 (estatico) com uma matriz com posicao relativa de tamanho 4x4 (peca em relacao a posx e posy)
+int une_matriz(int tela[10][24], int estatico[10][24], int peca[4][4], int posx, int posy) {
+  int cont0;
+  int cont1;
+
+  for(cont0 = 0; cont0 < 10; cont0++) {
+    
+    for(cont1 = 0; cont1 < 24; cont1++){
+      tela[cont0][cont1] = estatico[cont0][cont1];
+      
+      if (((cont0 - posx) >= 0) && ((cont1 - posy) >= 0) && ((cont0 - posy) < 4) && ((cont1- posy) < 4) && peca[(cont0 - posx)][(cont1 - posy)] != 0) {
+        tela[cont0][cont1] = peca[(cont0 - posx)][(cont1 - posy)];
+      }
+    }
+  }
+
+  return 0;
+}
+
+//Funcao que desenha uma matriz 10x24 em uma tela 320x240, preenchendo a altura da tela e centralizada na largura da mesma
 int desenha_matriz(int t[10][24]){
-    int cont0;
-    int cont1;
+  int cont0;
+  int cont1;
+  int posx1, posx2;
+  int posy1, posy2;
 
-    int posx1, posx2, posy1, posy2;
-    video_open();
-    video_clear();
-    for(cont0 = 0; cont0 < 10; cont0++)
-  {
-        for(cont1 = 0; cont1 < 24; cont1++){
-          posx1 = (cont0*10) + 110;
-          posy1 = (cont1*10) + 110;
-          posx2 = ((cont0+1)*10) + 109;
-          posy2 = ((cont1+1)*10) + 109;
-          if (t[cont0][cont1] == 0){
-            video_box(posx1, posy1, posx2, posy2, video_WHITE);
-          }
-          else if(t[cont0][cont1] == 1){
-            video_box(posx1, posy1, posx2, posy2, video_RED);
-          }
-        }
+  video_open();
+  video_clear();
+
+  for(cont0 = 0; cont0 < 10; cont0++) {
+    
+    for(cont1 = 0; cont1 < 24; cont1++){
+      posx1 = (cont0 * 10) + 110;
+      posy1 = (cont1 * 10);
+      posx2 = ((cont0 + 1) * 10 ) + 109;
+      posy2 = ((cont1 + 1) * 10) - 1;
+      
+      if (t[cont0][cont1] == 0) {
+        video_box(posx1, posy1, posx2, posy2, video_WHITE);
+      }
+      else if(t[cont0][cont1] == 1) {
+        video_box(posx1, posy1, posx2, posy2, video_RED);
+      }
+      else if(t[cont0][cont1] == 2) {
+        video_box(posx1, posy1, posx2, posy2, video_ORANGE);
+      }
+      else if(t[cont0][cont1] == 3) {
+        video_box(posx1, posy1, posx2, posy2, video_PINK);
+      }
+      else if(t[cont0][cont1] == 4) {
+        video_box(posx1, posy1, posx2, posy2, video_YELLOW);
+      }
+      else if(t[cont0][cont1] == 5) {
+        video_box(posx1, posy1, posx2, posy2, video_LIME);
+      }
+      else if(t[cont0][cont1] == 6) {
+        video_box(posx1, posy1, posx2, posy2, video_GREEN);
+      }
+      else if(t[cont0][cont1] == 7) {
+        video_box(posx1, posy1, posx2, posy2, video_LIGHT_BLUE);
+      }
+      else if(t[cont0][cont1] == 8) {
+        video_box(posx1, posy1, posx2, posy2, video_CYAN);
+      }
+      else if(t[cont0][cont1] == 9) {
+        video_box(posx1, posy1, posx2, posy2, video_BLUE);
+      }
+      else if(t[cont0][cont1] == 10) {
+        video_box(posx1, posy1, posx2, posy2, video_MAGENTA);
+      }
+      else if(t[cont0][cont1] == 11) {
+        video_box(posx1, posy1, posx2, posy2, video_PURPLE);
+      }
+      else if(t[cont0][cont1] == 12) {
+        video_box(posx1, posy1, posx2, posy2, video_BROWN);
+      }
+      else if(t[cont0][cont1] == 13) {
+        video_box(posx1, posy1, posx2, posy2, video_GRAY);
+      }
+      else if(t[cont0][cont1] == 14) {
+        video_box(posx1, posy1, posx2, posy2, video_LIGHT_GRAY);
+      }
+      else if(t[cont0][cont1] == 15) {
+        video_box(posx1, posy1, posx2, posy2, video_BLACK);
+      }
+    }
   }
+
   video_show();
   video_close();
+  
   return 0;
-  
 }
 
-int mover(posy){
-  posy++;
 
-  return pos[0];
-  
-}
+//Funcao que verifica a possibilidade de mover uma pecao de posicao
+int mover(int estatico[10][24], int peca[4][4], int posx, int posy, int dx, int dy) {
+  int cont0;
+  int cont1;
+  int result = 0;
 
-int somar_matriz(int tela[10[24], int matrixexibicao[10][24], int peca[3][3], int posx, int posy]){
-  for(cont0 = 0; cont0 < 10; cont0++)
-  {
-        for(cont1 = 0; cont1 < 24; cont1++){
+  for(cont0 = 0; cont0 < 4; cont0++) {
+    
+    for(cont1 = 0; cont1 < 4; cont1++) {
 
-          if (tela[cont0][cont1] != 0){
-            matrixexibicao[cont0][cont1] = tela[cont0][cont1];
-          }
-          else if(peca[cont0][cont1] != 0){
-            matrixexibicao[cont0][cont1] = tela[cont0][cont1];
-          }
-        }
+      //Condicional para verificar se o bloco atual da peca, caso preenchido, "sai" do limite da tela apos o deslocamento
+      //Se passar, significa que o movimento e impossivel
+      if ((peca[cont0][cont1] != 0) && (((cont0 + posx + dx) < 0) || ((cont1 + posy + dy) < 0) || ((cont0 + posx + dx) > 9) || ((cont1 + posy + dy) > 23))) {
+        result = 1;
+      }
+      //Condicional para verificar se o bloco atual da peca, caso preenchido, sobreporia os blocos ja estaticos apos o deslocamento
+      //Se passar, significa que o movimento e impossivel
+      else if ((peca[cont0][cont1] != 0) && (estatico[(cont0 + posx + dx)][(cont1 + posy + dy)] != 0)) {
+        result = 1;
+      }
+    }
   }
+
+  //Se nao houve obstrucao, ocorre o movimento (atualiza a posicao em relacao ao deslocamento requerido)
+  if(result == 0) {
+    posx = (posx + dx);
+    posy = (posy + dy);
+  }
+
+  //Retorna se a operacao foi bem-sucedida (0) ou nao (1)
+  return result;
 }
+
 
 int main ( void ) {
   int cont;
+
   int tela[10][24];
-  int peca[3][3];
+  int estatico[10][24];
+  int peca[4][4];
+
   int posx = 4;
   int posy = 0;
-  int cols;
+  
+  /*int cols;
   int rows;
   int tcols;
-  int trows;
+  int trows;*/
 
-  preenche_zero(tela);
+  //Inicia com 0 os espacos de jogo e da peca
+  preenche_zero_10_x_24(estatico);
+  preenche_zero_4_x_4(peca);
 
-  //Desenha a peça na tela
-  peca[0][0] = 1;
-  peca[0][1] = 1;
-  peca[0][2] = 1;
-  peca[1][0] = 1;
-  peca[1][1] = 0;
-  peca[1][2] = 0;
-  peca[2][0] = 0;
-  peca[2][1] = 0;
-  peca[2][2] = 0;
+  //Gera um indice de cor aleatorio entre 1 e 12
+  int rColor = ((rand() % 12) + 1);
+
+  //Preenche os espacos da matriz da peca com o indice da cor
+  peca[0][0] = rColor;
+  peca[0][1] = rColor;
+  peca[0][2] = rColor;
+  peca[1][0] = rColor;
   
+  preenche_zero_10_x_24(tela);
+  une_matriz(tela, estatico, peca, posx, posy);
   desenha_matriz(tela);
 
   //Movimenta a peça
-  for(cont = 0; cont < 13; cont++){
-    pos0[0] = mover(pos0, tela);
-    pos1[1] = mover(pos1, tela);
-    pos2[0]= mover(pos2, tela);
-    pos3[0] = mover(pos3, tela);
+  for(cont = 0; cont < 30; cont++){
+    mover(estatico, peca, posx, posy, 0, 1);
+    
+    preenche_zero_10_x_24(tela);
+    une_matriz(tela, estatico, peca, posx, posy);
     desenha_matriz(tela);
+    
     sleep(1);
   }
 
@@ -140,3 +231,18 @@ int main ( void ) {
   */
 return 0;
 }
+
+/*int imprime_tela(int t[10][20]){
+    int cont0;
+    int cont1;
+    for(cont0 = 0; cont0 < 10; cont0++)
+  {
+        for(cont1 = 0; cont1 < 20; cont1++){
+            printf("%d", t[cont0][cont1]);
+        }
+        printf("\n");
+  }
+  return 0;
+  
+}
+*/
