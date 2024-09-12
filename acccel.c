cca873fad613103f3173474ad9d9a4c22f22c63a
Pedro Cruz <pedrohenvcruz@gmail.com>
2024-09-12 17:30:32 -0300

    acelerômetro

---
+++ acccel.c
@@ -0,0 +1,21 @@
+#include <ADXL345.c>
+#include <stdio.h>
+
+int main(){
+    int16_t XYZ[3];
+
+    configure_pinmux();
+
+    I2C0_init();
+
+    ADXL345_Init();
+
+    while(1){
+        if(ADXL345_IsDataReady()){
+            ADXL345_XYZ_Read(XYZ)
+            printf("X=%d, Y=%d, Z=%d\n", XYZ[0], XYZ[1], XYZ[2]);
+        }
+    }
+
+    return 0;
+}
