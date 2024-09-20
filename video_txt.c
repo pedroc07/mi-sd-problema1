3037c06d66af58e81daf5e22d2c57dc061d981fc
pedroc07 <phvcruz@ecomp.uefs.br>
2024-09-20 15:44:03 -0300

    video text

--- video_txt.c
+++ video_txt.c
@@ -5,14 +5,15 @@
 # include <intelfpgaup/video.h>
 
 int main(){
-    char msg_buffer[80] = "game over";
+    char msg_buffer[9] = "game over";
     int screen_x, screen_y, char_x, char_y;
 
     video_open();
     video_read (&screen_x, &screen_y, &char_x, &char_y);
-    video_text (100, 150, msg_buffer);
+    video_text (char_x-strlen(msg_buffer), char_y - 1, msg_buffer);
     video_show ();
     video_close ();
     printf ("\nExiting sample program\n");
     return 0;
 }
+
