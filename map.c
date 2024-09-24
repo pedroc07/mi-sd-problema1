484c15a43c35a418b92760951c0bd79923f37ca8
Luc4ssz <145631570+Luc4ssz@users.noreply.github.com>
2024-09-24 14:02:33 -0300

    Add files via upload
    add calibrate.

--- map.c
+++ map.c
@@ -105,7 +105,7 @@ void I2C0_init(){
 }
 
 //Escreve um valor em um registrador
-void ADXL_write(uint8_t address, uint8_t value){
+void ADXL345_write(uint8_t address, uint8_t value){
     *I2C0_data = address + 0x400;
     *I2C0_data = value;
 }
@@ -149,23 +149,23 @@ len){
 
 void ADXL_345_init(){
     //+-16g range, full resolution
-    ADXL_write(ADXL345_REG_DATA_FORMAT, XL345_RANGE_16G | XL345_FULL_RESOLUTION);
+    ADXL345_write(ADXL345_REG_DATA_FORMAT, XL345_RANGE_16G | XL345_FULL_RESOLUTION);
 
     //taxa de aceleração: 200Hz
-    ADXL_write(ADXL345_REG_BW_RATE, XL345_RATE_200);
+    ADXL345_write(ADXL345_REG_BW_RATE, XL345_RATE_200);
 
-    ADXL_write(ADXL345_REG_THRESH_ACT, 0x04); //atividade
-    ADXL_write(ADXL345_REG_THRESH_INACT, 0x02); //inatividade
+    ADXL345_write(ADXL345_REG_THRESH_ACT, 0x04); //atividade
+    ADXL345_write(ADXL345_REG_THRESH_INACT, 0x02); //inatividade
 
-    ADXL_write(ADXL345_REG_TIME_INACT, 0x02); //tempo para inatividade
-    ADXL_write(ADXL345_REG_ACT_INACT_CTL, 0xFF);
-    ADXL_write(ADXL345_REG_INT_ENABLE, XL345_ACTIVITY | XL345_INACTIVITY );
+    ADXL345_write(ADXL345_REG_TIME_INACT, 0x02); //tempo para inatividade
+    ADXL345_write(ADXL345_REG_ACT_INACT_CTL, 0xFF);
+    ADXL345_write(ADXL345_REG_INT_ENABLE, XL345_ACTIVITY | XL345_INACTIVITY );
 
     // para a medição
-    ADXL_write(ADXL345_REG_POWER_CTL, XL345_STANDBY);
+    ADXL345_write(ADXL345_REG_POWER_CTL, XL345_STANDBY);
 
     // começa a medição
-    ADXL_write(ADXL345_REG_POWER_CTL, XL345_MEASURE);
+    ADXL345_write(ADXL345_REG_POWER_CTL, XL345_MEASURE);
 }
 
 void ADXL345_XYZ_Read(int16_t szData16[3]){
@@ -204,6 +204,97 @@ int ADXL345_IsDataReady(){
     return bReady;
 }
 
+int ROUNDED_DIVISION(int n, int d){
+    int result;
+
+    if(n >= 0){
+        result = (n + (d / 2)) / d;
+    }else{
+        result = (n - (d / 2)) / d;
+    }
+
+    return result;
+}
+
+
+void ADXL345_Calibrate(){
+    
+    int average_x = 0;
+    int average_y = 0;
+    int average_z = 0;
+    int16_t XYZ[3];
+    int8_t offset_x;
+    int8_t offset_y;
+    int8_t offset_z;
+    printf("================================================\n");
+    printf("Iniciando a calibragem do ADXL345\n");
+    
+    // Coloca em estado de STANDBY
+    ADXL345_write(ADXL345_REG_POWER_CTL, XL345_STANDBY);
+    
+    // Obtém os offsets atuais
+    ADXL345_read(ADXL345_REG_OFSX, (uint8_t *)&offset_x);
+    ADXL345_read(ADXL345_REG_OFSY, (uint8_t *)&offset_y);
+    ADXL345_read(ADXL345_REG_OFSZ, (uint8_t *)&offset_z);
+    
+    // Usa uma taxa de 100 Hz para a aclibração e salva a taxa atual
+    uint8_t saved_bw;
+    ADXL345_read(ADXL345_REG_BW_RATE, &saved_bw);
+    ADXL345_write(ADXL345_REG_BW_RATE, XL345_RATE_100);
+    
+    // Usa 16g de range (resolução total) e salva o formato atual.
+    uint8_t saved_dataformat;
+    ADXL345_read(ADXL345_REG_DATA_FORMAT, &saved_dataformat);
+    ADXL345_write(ADXL345_REG_DATA_FORMAT, XL345_RANGE_16G | XL345_FULL_RESOLUTION);
+    
+    // Coloca em estado de MEASURE (medição)
+    ADXL345_write(ADXL345_REG_POWER_CTL, XL345_MEASURE);
+    
+    // Obtém as acelerações médias dos eixos x, y e z por 32 amostras(LSB 3.9 mg)
+    int i = 0;
+    while (i < 32){
+        if (ADXL345_IsDataReady()){
+            ADXL345_XYZ_Read(XYZ);
+            average_x += XYZ[0];
+            average_y += XYZ[1];
+            average_z += XYZ[2];
+            i++;
+        }
+    }
+    average_x = ROUNDED_DIVISION(average_x, 32);
+    average_y = ROUNDED_DIVISION(average_y, 32);
+    average_z = ROUNDED_DIVISION(average_z, 32);
+    
+    // Coloca em estado de STANDBY
+    ADXL345_write(ADXL345_REG_POWER_CTL, XL345_STANDBY);
+
+    printf("\nAverage X=%d, Y=%d, Z=%d\n", average_x, average_y, average_z);
+    
+    // Calcula os offsets (LSB 15.6 mg)
+    offset_x += ROUNDED_DIVISION(0-average_x, 4);
+    offset_y += ROUNDED_DIVISION(0-average_y, 4);
+    offset_z += ROUNDED_DIVISION(256-average_z, 4);
+
+    printf("Calibration: offset_x: %d, offset_y: %d, offset_z: %d (LSB: 15.6 mg)\n",offset_x,offset_y,offset_z);
+    
+    // "Seta" os registradores dos offsets
+    ADXL345_write(ADXL345_REG_OFSX, offset_x);
+    ADXL345_write(ADXL345_REG_OFSY, offset_y);
+    ADXL345_write(ADXL345_REG_OFSZ, offset_z);
+    
+    // Volta para a taxa bw original
+    ADXL345_write(ADXL345_REG_BW_RATE, saved_bw);
+    
+    // Volta para o formato de dado original
+    ADXL345_write(ADXL345_REG_DATA_FORMAT, saved_dataformat);
+    
+    // Coloca em estado de MEASURE (medição)
+    ADXL345_write(ADXL345_REG_POWER_CTL, XL345_MEASURE);
+
+    printf("\nFinalizando a calibragem do ADXL345\n");
+    printf("================================================\n");
+}
+
 
 int main(void) {
     int fd = -1;
@@ -231,9 +322,9 @@ int main(void) {
 
    //Mapeamento do multiplexador
    /*
-   I2C0USEFPGA = (int *) (SYSMGR_virtual + SYSMGR_I2C0USEFPGA);
-   GENERALIO7 = (int *) (SYSMGR_virtual + SYSMGR_GENERALIO7);
-   GENERALIO8 = (int *) (SYSMGR_virtual + SYSMGR_GENERALIO8);
+    I2C0USEFPGA = (int *) (SYSMGR_virtual + SYSMGR_I2C0USEFPGA);
+    GENERALIO7 = (int *) (SYSMGR_virtual + SYSMGR_GENERALIO7);
+    GENERALIO8 = (int *) (SYSMGR_virtual + SYSMGR_GENERALIO8);
    */
 
     //Configurações iniciais
@@ -249,13 +340,15 @@ int main(void) {
         
         ADXL_345_init();
 
+        ADXL345_Calibrate();
+
         int x = 0;
 
         while(x < 10){
         if(ADXL345_IsDataReady()){
             ADXL345_XYZ_Read(XYZ);
-                printf("X=%d, Y=%d, Z=%d\n", XYZ[0], XYZ[1], XYZ[2]);
-                usleep(1000000);
+            printf("X=%d, Y=%d, Z=%d\n", XYZ[0], XYZ[1], XYZ[2]);
+            usleep(1000000);
             x++;
             }
         }
